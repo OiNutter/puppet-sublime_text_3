85f6b0f751d9b8a25d2dedf070723346e18e1a49
OiNutter <willmckenzie@oinutter.co.uk>
2014-03-15 16:40:11 +0000

    Attempting to fix tests

--- spec/classes/sublime_text_3_spec.rb
+++ spec/classes/sublime_text_3_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'sublime_text_3' do
   it do
-    should contain_package('SublimeText3').with({
+    should contain_package('SublimeText').with({
       :provider => 'appdmg',
       :source   => 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203059.dmg'
     })
